9f994a5a457791b9582514035efc275e0167071d
brnomade <36184011+brnomade@users.noreply.github.com>
2020-07-20 14:41:56 +0100

    fixes and refactoring

--- abstract_emiter.py
+++ abstract_emiter.py
@@ -6,14 +6,17 @@ class AbstractEmitter:
         self.header = ""
         self.code = ""
 
-    def emit(self, code):
-        self.code += code
+    def emit(self, input_source):
+        self.code += input_source
 
-    def emit_line(self, code):
-        self.code += code + '\n'
+    def emit_line(self, input_source):
+        self.code += input_source + '\n'
 
-    def header_line(self, code):
-        self.header += code + '\n'
+    def emit_header(self, input_source):
+        self.code += input_source
+
+    def emit_header_line(self, input_source):
+        self.header += input_source + '\n'
 
     def write_file(self):
         with open(self.full_path, 'w') as outputFile:
@@ -23,50 +26,51 @@ class AbstractEmitter:
 class CEmitter(AbstractEmitter):
 
     def emit_action_0(self):
-        self.header_line("#include <stdio.h>")
-        self.header_line("#include <stdbool.h>")
+        self.emit_header_line("#include <stdio.h>")
+        self.emit_header_line("#include <stdbool.h>")
 
     def emit_action_1(self):
         self.emit_line("int main(void){")
 
-    def emit_action_2(self, a_constant_symbol):
-        #
-        if a_constant_symbol.type in ["CHARACTER", "STRING"]:
+    def emit_action_2(self, a_symbol):
+
+        if a_symbol.type in ["CHARACTER", "STRING"]:
             ctype = "unsigned char"
-            cvalue = a_constant_symbol.value.strip("'").strip('"')
-        elif a_constant_symbol.type == "SIGNED_DECIMAL":
+            cvalue = a_symbol.value.strip("'").strip('"')
+        elif a_symbol.type == "SIGNED_DECIMAL":
             ctype = "int"
-            cvalue = a_constant_symbol.value
-        elif a_constant_symbol.type == "UNSIGNED_DECIMAL":
+            cvalue = a_symbol.value
+        elif a_symbol.type == "UNSIGNED_DECIMAL":
             ctype = "unsigned int"
-            cvalue = a_constant_symbol.value
-        elif a_constant_symbol.type in ["SIGNED_REAL", "UNSIGNED_REAL"]:
+            cvalue = a_symbol.value
+        elif a_symbol.type in ["SIGNED_REAL", "UNSIGNED_REAL"]:
             ctype = "float"
-            cvalue = a_constant_symbol.value
-        elif a_constant_symbol.type in ["CONSTANT_TRUE", "CONSTANT_FALSE"]:
+            cvalue = a_symbol.value
+        elif a_symbol.type in ["CONSTANT_TRUE", "CONSTANT_FALSE"]:
             ctype = "_Bool"
-            cvalue = "true" if a_constant_symbol.type == "CONSTANT_TRUE" else "false"
-        elif a_constant_symbol.type == "NUMBER_HEXADECIMAL":
+            cvalue = "true" if a_symbol.type == "CONSTANT_TRUE" else "false"
+        elif a_symbol.type == "NUMBER_HEXADECIMAL":
             ctype = "unsigned short"
-            cvalue = "0x{0}".format(a_constant_symbol.value.upper().strip("&H"))
-        elif a_constant_symbol.type == "NUMBER_OCTAL":
+            cvalue = "0x{0}".format(a_symbol.value.upper().strip("&H"))
+        elif a_symbol.type == "NUMBER_OCTAL":
             ctype = "unsigned short"
-            cvalue = "0{0}".format(a_constant_symbol.value.upper().strip("&O"))
-        elif a_constant_symbol.type == "NUMBER_BINARY":
+            cvalue = "0{0}".format(a_symbol.value.upper().strip("&O"))
+        elif a_symbol.type == "NUMBER_BINARY":
             ctype = "unsigned short"
-            cvalue = "0b{0}".format(a_constant_symbol.value.upper().strip("&B"))
+            cvalue = "0b{0}".format(a_symbol.value.upper().strip("&B"))
         else:
-            ctype = a_constant_symbol.type
-            cvalue = a_constant_symbol.value
-        #
-        if a_constant_symbol.type in ["STRING"]:
+            cvalue = a_symbol.value
+            ctype = a_symbol.type
+
+        if a_symbol.type in ["STRING"]:
             line = "const {0} {1} [ ] = \"{2}\";"
-        elif a_constant_symbol.type in ["CHARACTER"]:
+        elif a_symbol.type in ["CHARACTER"]:
             line = "const {0} {1} = '{2}';"
         else:
             line = "const {0} {1} = {2};"
-        #
-        self.header_line(line.format(ctype, a_constant_symbol.name.upper(), cvalue))
+
+        self.emit_header_line(line.format(ctype, a_symbol.name, cvalue))
+
 
     def emit_action_3(self, a_variable_symbol):
         #
@@ -83,7 +87,7 @@ class CEmitter(AbstractEmitter):
             ctype = "unsigned char"
         #
         line = "{0} {1};"
-        self.header_line(line.format(ctype, a_variable_symbol.name))
+        self.emit_header_line(line.format(ctype, a_variable_symbol.name))
 
     def emit_action_5(self):
         self.emit_line("return 0;")
@@ -101,13 +105,14 @@ class CEmitter(AbstractEmitter):
 
     def emit_action_7(self, step):
         if step == 1:
-            line = "repeat"
+            line = "do"
             self.emit_line(line)
+            self.emit_line("{")
         elif step == 2:
-            line = "until "
+            line = "} while (!("
             self.emit(line)
         elif step == 3:
-            line = ";"
+            line = "));"
             self.emit_line(line)
         else:
             raise KeyError
