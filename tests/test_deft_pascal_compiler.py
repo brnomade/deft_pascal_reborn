071aada293871c19c60f47c3a6c4c56b9e3b85cb
brnomade <36184011+brnomade@users.noreply.github.com>
2020-07-22 23:13:21 +0100

    improvements on unit tests framework, now integrating to clang and testing the c compilation output for errors.

--- tests/test_deft_pascal_compiler.py
+++ tests/test_deft_pascal_compiler.py
@@ -4,8 +4,24 @@ from parameterized import parameterized
 from common_test_cases import LanguageTests, TestSuit
 import inspect
 import logging
+import os
+import subprocess
+import sys
+
 
 logger = logging.getLogger(__name__)
+glb_output_filename = "my_scenario_program.c"
+param_remove_file_after_test = False
+
+
+def safe_path( in_str ):
+    # function to handle windows path names with empty spaces
+    # in linux or ios this function is neutral
+    if sys.platform.startswith('win'):
+        out_str = '"' + in_str + '"'
+    else:
+        out_str = in_str
+    return out_str
 
 
 class TestDeftPascalCompiler(TestCase):
@@ -19,6 +35,31 @@ class TestDeftPascalCompiler(TestCase):
             logger.warning(msg.format(set(available_tests)-set(selected_tests)))
             # print("\n\nWARNING!\nNot all test scenarios are being run. Review TestSuit class\n\n")
 
+    def tearDown(self):
+        if os.path.exists(glb_output_filename):
+            if param_remove_file_after_test:
+                os.remove(glb_output_filename)
+        else:
+            print("can't find file", glb_output_filename, ". will not delete it.")
+
+    @staticmethod
+    def compile_in_clang():
+        clang_path = os.path.join("C:\\", "Program Files", "LLVM", "bin", "clang.exe")
+        oscmd = safe_path(clang_path)
+        oscmd = oscmd + " -fsyntax-only --analyzer-output text -E -x c"
+        oscmd = oscmd + " " + glb_output_filename
+        oscmd = oscmd + " " + "> out.txt 2> out2.txt"
+        print(oscmd)
+        subprocess.run(oscmd, shell=True)
+
+        file = open("out.txt")
+        result = file.read()
+        file.close()
+
+        file = open("out2.txt")
+        result = result + file.read()
+        file.close()
+        return result
 
     @parameterized.expand(LanguageTests.compiler_tests_to_run())
     def test(self, name, source_code):
@@ -29,6 +70,11 @@ class TestDeftPascalCompiler(TestCase):
         #
         error_log = compiler.compile(ast)
         self.assertEqual(error_log, [])
+        #
+        output = self.compile_in_clang()
+        print(output)
+        self.assertNotIn("error", output)
+
 
 
 
