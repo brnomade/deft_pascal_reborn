6855de32a7706d7b7117b2aab2b90418f15afa00
brnomade <36184011+brnomade@users.noreply.github.com>
2020-08-15 17:32:51 +0100

    fixes to enable running in TravisCI, Windows and Linux

--- tests/test_code_generation.py
+++ tests/test_code_generation.py
@@ -63,7 +63,7 @@ class TestCodeGenerator(TestCase):
         c_compiler = "{0} -o {1}".format(gcc_name, output_exe)
         c_env = "{0} {1} > {2} 2> {3}".format(c_compiler, input_source, output_out, output_err)
 
-        print(c_env)
+        GLB_LOGGER.debug(c_env)
         #
         if platform.system() == "Windows":
             os.chdir(mig_dir)
@@ -96,12 +96,19 @@ class TestCodeGenerator(TestCase):
         return result_out + result_err
 
     @staticmethod
-    def run_in_shell(input_c):
-        home_dir = os.getcwd()
-        bin_path = "output\\bin"
-        output_exe = os.path.join(home_dir, bin_path, input_c.split(".")[0] + ".exe")
-        c_env = "{0}".format(output_exe)
-        return subprocess.run(c_env, shell=True)
+    def run_in_shell(c_file_filename):
+        #
+        filename = os.path.basename(c_file_filename)
+        filepath = os.path.dirname(c_file_filename)
+        #
+        filename = filename.split(".")[0] + ".exe"
+        #
+        executable_file = os.path.join(filepath, filename)
+        #
+        msg = "{0} does not exist or cannot be found at {1}"
+        if not os.path.isfile(executable_file):
+            GLB_LOGGER.error(msg.format("Compiler executable", executable_file))
+        return subprocess.run(executable_file)
 
 
     @parameterized.expand(LanguageTests.generator_tests_to_run())
@@ -116,7 +123,7 @@ class TestCodeGenerator(TestCase):
             GLB_LOGGER.error(error_log)
         self.assertIsNone(error_log)
         #
-        GLB_LOGGER.error(compiler.ast.pretty())
+        GLB_LOGGER.debug(compiler.ast.pretty())
         #
         error_log = compiler.compile()
         if error_log:
@@ -124,7 +131,7 @@ class TestCodeGenerator(TestCase):
         self.assertIsNone(error_log)
         #
         ic = compiler.intermediate_code
-        GLB_LOGGER.error(ic)
+        GLB_LOGGER.debug(ic)
         #
         output_code = compiler.generate()
         self.assertIsNotNone(output_code)
@@ -136,8 +143,8 @@ class TestCodeGenerator(TestCase):
         filename = os.path.join(filepath, "{0}.c".format(name))
 
         if output_code:
-            GLB_LOGGER.error(output_code)
-            GLB_LOGGER.error(filename)
+            GLB_LOGGER.debug(output_code)
+            GLB_LOGGER.debug(filename)
             file = open(filename, "w")
             file.write(output_code)
             file.close()
